26d30186e1072cfa3a0c709c51570b2526f3eadf
bugsywap <ollerogabe@gmail.com>
2024-04-09 21:19:08 +0800

    Undid a wrong function in BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,7 +9,7 @@ class BookController extends Controller
 {
     public function index(){
         $booksTable = book::all();
-
-        return view('index', compact('booksTable'));
+        
+        return view('index' , ['book' => $booksTable]);
     }
 }
