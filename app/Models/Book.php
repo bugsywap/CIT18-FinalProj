412d1a37ceac28b96dc646a005d844632265a195
bugsywap <ollerogabe@gmail.com>
2024-04-09 20:24:05 +0800

    Added Book.php in models

---
+++ app/Models/Book.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id' , 'isbn' , 'title' , 'author' , 'description' , 'date_'
+    ];
+}
